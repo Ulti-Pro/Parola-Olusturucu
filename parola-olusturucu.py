36f1090591600ddeb3c479eea6808c63f47c96de
Ulti-Pro <gurlekmehmetbartu@gmail.com>
2025-02-06 20:33:08 +0300

    Create parola-olusturucu.py
    Güçlü parolalar oluşturan kod

---
+++ parola-olusturucu.py
@@ -0,0 +1,17 @@
+import random
+
+# 1. Adım: Karakter kümesi
+karaterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+# 2. Adım: Kullanıcıdan parola uzunluğunu alma
+parola_uzunlugu = int(input("Parolanın uzunluğunu girin: "))
+
+# 3. Adım: Parola için boş bir değişken oluşturma
+parola = ""
+
+# 4. Adım: Döngü ile rastgele karakter seçip parolayı oluşturma
+for i in range(parola_uzunlugu):
+    parola += random.choice(karakterler)
+
+# 5. Adım: Sonucu yazdırma
+print("Oluşturduğunuz parola: ", parola)
